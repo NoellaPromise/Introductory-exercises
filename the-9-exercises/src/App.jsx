4d47470599115792dd1b3cb137339cbf33740060
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-05 15:45:08 +0200

    delete App.css

--- the-9-exercises/src/App.jsx
+++ the-9-exercises/src/App.jsx
@@ -1,14 +1,9 @@
-
-import './App.css'
-
 function App() {
-
-
   return (
     <div className="bg-orange-300 h-screen w-screen flex justify-center rounded-xl">
-          <h1 className="p-8 text-4xl">Hello, World!</h1>
-      </div>
-  )
+      <h1 className="p-8 text-4xl">Hello, World!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
