c65721b9c351e7d6c1c4946cf1c63020f500f9b5
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-05 08:14:19 +0200

    Add exercise2

--- the-9-exercises/src/App.jsx
+++ the-9-exercises/src/App.jsx
@@ -1,14 +1,20 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  function handleClick() {
+    alert("Clicked!");
+  }
 
   return (
-    <>
-      <h1 className='bg-red-400'>hey there</h1>
-    </>
-  )
+    <div className="bg-orange-300 h-screen flex justify-center items-center ">
+      <button
+        className="px-16 py-5 bg-green-900 hover:bg-slate-400 active:bg-slate-800 text-amber-100 rounded-lg"
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
