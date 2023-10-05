d8e764d55d11ad90a4e093360a1781d895f3b29e
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-05 08:30:14 +0200

    Add exercis4

--- the-9-exercises/src/App.jsx
+++ the-9-exercises/src/App.jsx
@@ -1,14 +1,22 @@
-
-import './App.css'
-
+import List from "./components/List";
 function App() {
-
+  const arrayOfItems = [
+    "dog  ",
+    "cat  ",
+    "chicken  ",
+    "cow  ",
+    "sheep  ",
+    "horse  ",
+  ];
+  const arrayOfComponents = arrayOfItems.map((element, index) => (
+    <List key={index} element={element} />
+  ));
 
   return (
-    <>
-      <h1 className='bg-red-400'>hey there</h1>
-    </>
-  )
+    <ul className="text-2xl mx-40 my-10 p-10 bg-green-900  text-white rounded-lg list-disc">
+      {arrayOfComponents}
+    </ul>
+  );
 }
 
-export default App
+export default App;
