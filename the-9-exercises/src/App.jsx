6501385c732e52c544ad4d8be1d00562ba1f61d3
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-10-05 16:13:36 +0200

    Remove spaces

--- the-9-exercises/src/App.jsx
+++ the-9-exercises/src/App.jsx
@@ -1,20 +1,10 @@
-
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import AnimalList from "./components/AnimalList";
 import data from "./components/data.json";
 
 export default function App() {
-  const ANIMAL = data.map(item => (
+  const ANIMAL = data.map((item) => (
     <AnimalList id={uuidv4()} name={item.name} image={item.url} />
   ));
   return <div>{ANIMAL}</div>;
 }
-
-
-
-
-
-
-
-  
-   
